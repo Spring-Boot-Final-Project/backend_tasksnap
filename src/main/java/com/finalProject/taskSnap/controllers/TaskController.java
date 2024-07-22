906b01f27ea578da7bcfac73cde4d2bd7da265d7
Min-Hung-Hsieh <hsiinung@gmail.com>
2024-07-21 20:22:09 -0400

    Add Post, Update, Delete Method in TaskService and TaskController classes

--- src/main/java/com/finalProject/taskSnap/controllers/TaskController.java
+++ src/main/java/com/finalProject/taskSnap/controllers/TaskController.java
@@ -2,10 +2,10 @@ package com.finalProject.taskSnap.controllers;
 
 import com.finalProject.taskSnap.models.Tasks;
 import com.finalProject.taskSnap.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TaskController {
@@ -17,6 +17,34 @@ public class TaskController {
 
     @GetMapping("/")
     public List<Tasks> homepage(){
+
         return taskService.getAllTask();
     }
+
+    // List task by id
+    @GetMapping("/{id}")
+    public Optional<Tasks> getTaskById(@PathVariable int id){
+        return taskService.getTaskById(id);
+    }
+
+    // Create task
+    @PostMapping("/create")
+    public int createTask(@RequestBody Tasks task){
+        return taskService.saveTask(task);
+    }
+
+    // Update task
+    @PutMapping("/update/{id}")
+    public int updateTask(@PathVariable int id, @RequestBody Tasks task){
+        task.setId(id);
+        return taskService.updateTask(task);
+    }
+
+    // Delete task
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable int id){
+        taskService.deleteTask(id);
+    }
+
+
 }
--- src/main/java/com/finalProject/taskSnap/services/TaskService.java
+++ src/main/java/com/finalProject/taskSnap/services/TaskService.java
@@ -5,21 +5,49 @@ import com.finalProject.taskSnap.repositories.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
     private final TaskRepository taskRepository;
 
     public TaskService(TaskRepository taskRepository){
+
         this.taskRepository = taskRepository;
     }
 
+
+    // Save tasks
     public int saveTask(Tasks task){
         taskRepository.save(task);
         return 1;
     }
 
+    // List All tasks
     public List<Tasks> getAllTask(){
+
         return taskRepository.findAll();
     }
+
+    // List task by id
+    public Optional<Tasks> getTaskById(int id){
+        return taskRepository.findById(id);
+    }
+
+    // Update tasks
+    public int updateTask(Tasks task){
+        if(taskRepository.existsById(task.getId())){
+            taskRepository.save(task);
+            return 1;
+        } else {
+            return 0;
+        }
+
+    }
+
+    // Delete tasks
+    public void deleteTask(int id){
+        taskRepository.deleteById(id);
+    }
+
 }
